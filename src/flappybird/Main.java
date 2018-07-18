3227e71bdbbf1d7e6bb7ad2568a5319853e4a622
hienvdse63056 <41386763+hienvdse63056@users.noreply.github.com>
2018-07-19 00:00:35 +0700

    Update Main.java

--- src/flappybird/Main.java
+++ src/flappybird/Main.java
@@ -65,10 +65,7 @@ public class Main extends JFrame implements KeyListener, MouseListener {
 //
     }
 
-    public static void main(String[] args) {
-        Main m = new Main()
-
-    }
+   
 
     @Override
     public void keyTyped(KeyEvent e) {
